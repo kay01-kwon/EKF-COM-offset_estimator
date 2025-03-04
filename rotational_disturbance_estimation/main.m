055bd5fd1388cad2bd63e12288c6cc2fe4fe2558
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2025-03-04 21:44:07 +0900

    Update main.m

--- rotational_disturbance_estimation/main.m
+++ rotational_disturbance_estimation/main.m
@@ -13,9 +13,11 @@ s_init = [1, 0, 0, 0, ...
           0, 0, 0]';
 s_true_vec = zeros(7,N);
 
-Param.J = 0.02*eye(3);
+Param.J = 0.04*eye(3);
 Param.rx = 0.020;
 Param.ry = 0;
+
+Param_est.J = 0.02*eye(3);
 
 u = [1, 0., 0, 0]';
 d = [0, 0, 0]';
@@ -34,7 +36,19 @@ s_prior = [1, 0, 0, 0, ...
 s_est_vec = zeros(10, N);
 s_est_vec(:,1) = s_prior;
 Cov_prior = 0.01^2*eye(10);
-Cov_prior(8:10,8:10) = 0.1*eye(3);
+Cov_prior(8:10,8:10) = 1*eye(3);
+var_tau_vec = zeros(3,N);
+var_tau_vec(:,1) = [Cov_prior(8,8);
+                    Cov_prior(9,9);
+                    Cov_prior(10,10)];
+
+std_noise = [0.01^2;
+             0.01^2;
+             0.01^2;
+             0.01^2;
+             0.01^2;
+             0.01^2;
+             0.01^2;];
 
 K_p = 3*eye(3);
 K_d = 1*eye(3);
@@ -48,6 +62,11 @@ for i = 1:N-1
         0;
         0];
 
+    if time(i) >= 3
+        d(3) = 1;
+    end
+
+
     r_cross_f = [ry*u(1);
                 -rx*u(1);
                 0];
@@ -56,22 +75,28 @@ for i = 1:N-1
     
     s = rk4_ode(@(t,s) rotational_dynamics(s, u, d, Param), s_init, time(i+1), time(i));
     
-    % q_nom = quat_normalize(s(1:4));
-    % s(1:4) = q_nom;
+    q_nom = quat_normalize(s(1:4));
+    s(1:4) = q_nom;
     s_true_vec(:,i+1) = s;
     s_init = s;
 
     dt = time(i+1) - time(i);
 
-    obs = s;
+    obs = s + normrnd(0, std_noise);
 
-    [s_pred, Cov_pred] = predictEKF(s_prior, Cov_prior, u, dt, Param);
+    [s_pred, Cov_pred] = predictEKF(s_prior, Cov_prior, u, dt, Param_est);
     [s_posterior, Cov_posterior] = measModelEKF(s_pred, Cov_pred, obs);
 
     s_est_vec(:,i+1) = s_posterior;
 
     Cov_prior = Cov_posterior;
     s_prior = s_posterior;
+
+    var_tau = [Cov_posterior(8,8);
+               Cov_posterior(9,9);
+               Cov_posterior(10,10)];
+
+    var_tau_vec(:,i+1) = var_tau;
 
     d_est = [s_posterior(8), s_posterior(9), s_posterior(10)]';
 
@@ -142,3 +167,15 @@ subplot(3,1,3)
 plot(time, d_true_vec(3,:))
 hold on
 plot(time, s_est_vec(10,:),'--', LineWidth=2)
+
+
+% Variance of lumped disturbance
+figure(4)
+subplot(3,1,1)
+plot(time, var_tau_vec(1,:))
+
+subplot(3,1,2)
+plot(time, var_tau_vec(2,:))
+
+subplot(3,1,3)
+plot(time, var_tau_vec(3,:))
